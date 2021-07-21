283e78ffea79f8f395d664f4c890ccd3c9ce9d17
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-21 12:19:10 +0900

    Add hash-map

--- hash-map.lisp
+++ hash-map.lisp
@@ -4,4 +4,31 @@
 
 (in-package #:hash-map)
 
+(defstruct (hash-map (:constructor %make-hash-map))
+  (table (make-hash-table :test #'eql)))
+
+(defmethod inc! ((hash-map hash-map)
+                 key
+                 value)
+  (with-slots (table) hash-map
+    (incf (gethash key table 0) value)))
+
+(defmethod dec! ((hash-map hash-map)
+                 key
+                 value)
+  (with-slots (table) hash-map
+    (decf (gethash key table 0) value)))
+
+(defmethod query ((hash-map hash-map)
+                  key)
+  (gethash key (hash-map-table hash-map) 0))
+
+#+nil
+(let ((m (%make-hash-map)))
+  (inc! m 0 1)
+  (inc! m 1 3)
+  (assert (= 3 (query m 1)))
+  (dec! m 1 1)
+  (assert (= 2 (query m 1))))
+
 (in-package #:cl-user)
