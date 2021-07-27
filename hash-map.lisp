c2b790c31af273f76e55548c78e1e39855bfd3a8
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-27 13:09:41 +0900

    Rename package

--- hash-map.lisp
+++ hash-map.lisp
@@ -1,39 +1,39 @@
-(defpackage hash-map
+(defpackage counter
   (:use #:cl)
   (:nicknames #:m)
-  (:export #:make-hash-map
+  (:export #:make-counter
            #:inc!
            #:dec!
            #:query))
 
-(in-package #:hash-map)
+(in-package #:counter)
 
-(defstruct (hash-map (:constructor make-hash-map (&key
-                                                    (test #'eql)
-                                                    (size 100)
-                                                    (default-value 0))))
+(defstruct (counter (:constructor make-counter (&key
+                                                  (test #'eql)
+                                                  (size 100)
+                                                  (default-value 0))))
   (table (make-hash-table :test test :size size))
   (default-value default-value))
 
-(defmethod inc! ((hash-map hash-map)
+(defmethod inc! ((counter counter)
                  key
                  value)
-  (with-slots (table default-value) hash-map
+  (with-slots (table default-value) counter
     (incf (gethash key table default-value) value)))
 
-(defmethod dec! ((hash-map hash-map)
+(defmethod dec! ((counter counter)
                  key
                  value)
-  (with-slots (table default-value) hash-map
+  (with-slots (table default-value) counter
     (decf (gethash key table default-value) value)))
 
-(defmethod query ((hash-map hash-map)
+(defmethod query ((counter counter)
                   key)
-  (with-slots (table default-value) hash-map
+  (with-slots (table default-value) counter
     (gethash key table default-value)))
 
 #+nil
-(let ((m (%make-hash-map)))
+(let ((m (make-counter)))
   (inc! m 0 1)
   (inc! m 1 3)
   (assert (= 3 (query m 1)))
