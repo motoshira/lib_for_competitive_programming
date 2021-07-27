b274549cb2e1ca9e6ff7172cbfc15ce45c6cd3b3
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-28 00:11:15 +0900

    Move tests

--- test/treap.lisp
+++ test/treap.lisp
@@ -1,51 +1,91 @@
 (ql:quickload :rove :slient t)
 
 (defpackage :test/treap
-  (:use #:cl
-        #:treap)
+  (:use #:cl #:treap)
   (:import-from #:rove))
 
 (in-package :test/treap)
 
-(let ((rove:*enable-colors* nil))
-  (let ((l (treap::make-treap 1 :sum 1))
-        (r (treap::make-treap 2 :sum 2))
-        (xx (treap::list->treap (list 2 1)))
-        (yy (treap::list->treap (list 4 3)))
-        (ex1 (treap::list->treap (list 1 4 2 3)))
-        (xs (list 2 4 3 1 4)))
-    (prove:is (treap::%plus-cnt l r) 2)
-    (prove:is (treap::%plus-sum l r) 3)
-    (prove:is (treap::%plus-cnt l nil) 1)
-    (prove:is (treap::%plus-sum l nil) 1)
-    ;; merge
-    (prove:is (treap::treap->list (treap::merge xx yy))
-              '(2 1 4 3))
+#+swank
+(rove:deftest test-treap
+  (let* ((xs (loop repeat 5 collect (random 100)))
+         (ys (loop repeat 10 collect (random 100)))
+         (zs (loop repeat 20 collect (random 100)))
+         (ws (list 1 3 5 7 10))  ; sum = 26
+         (rs (list 1 5 4 3 2))   ; sum = 15
+         (xs-treap (list->treap xs))
+         (ys-treap (list->treap ys))
+         (zs-treap (list->treap zs))
+         (ws-treap (list->treap ws))
+         (rs-treap (list->treap rs))
+         (null-treap (list->treap nil)))
+    (rove:testing "Testing equality"
+      (flet ((convert (list)
+               (treap->list (list->treap list))))
+        (rove:ok (equal (convert xs)
+                        xs))))
+    (rove:testing "get-cnt"
+      (rove:ok (= (%get-cnt ys-treap)
+                  10)
+               "normal")
+      (rove:ok (= (%get-cnt null-treap)
+                  0)
+               "null-treap"))
+    (rove:testing "get-sum"
+      (rove:ok (= (%get-sum ws-treap)
+                  26))
+      (rove:ok (= (%get-sum null-treap)
+                  0)
+               "null-treap"))
+    (rove:testing "plus-cnt"
+      (rove:ok (= (%plus-cnt xs-treap
+                             ys-treap)
+                  15)
+               "normal")
+      (rove:ok (= (%plus-cnt xs-treap
+                             null-treap)
+                  5)
+               "one is null"))
+    (rove:testing "plus-sum"
+      (rove:ok (= (%plus-sum ws-treap rs-treap)
+                  41)
+               "normal")
+      (rove:ok (= (%plus-sum ws-treap null-treap)
+                  26)
+               "one is null"))
+    (rove:testing "merge"
+      (rove:ok (equal (treap->list (merge ws-treap rs-treap))
+                      (concatenate 'list ws rs))
+               "Merge two treap preserving order"))
+    (rove:testing "split"
+      (rove:ok (every #'equal
+                      (mapcar #'treap->list
+                              (multiple-value-list
+                               (split ws-treap 2)))
+                      '((1 3)
+                        (5 7 10)))
+               "Split at 2")
+      (rove:ok (every #'equal
+                      (mapcar #'treap->list
+                              (multiple-value-list
+                               (split ws-treap 3)))
+                      '((1 3 5)
+                        (7 10)))
+               "Split at 3"))
+    (rove:testing "insert"
+      (rove:ok (equal (treap->list (insert ws-treap
+                                           2
+                                           11))
+                      '(1 3 11 5 7 10))
+               "Insert 11 at 2")
+      (rove:ok (equal (treap->list (insert ws-treap
+                                           3
+                                           11))
+                      '(1 3 5 11 7 10))
+               "Insert 11 at 3"))
+    #+nil
+    (rove:testing "remove"
+      (rove:ok (equal (treap->list (remove ws-treap 2))
+                      '(1 3 7 10))))))
 
-    ;; split
-    (prove:is (multiple-value-bind (l r)
-                  (treap::split ex1 2)
-                (list (treap::treap->list l)
-                      (treap::treap->list r)))
-              '((1 4)
-                (2 3)))
-
-    ;; conversion
-    (prove:is (treap::treap->list (treap::list->treap xs))
-              xs)
-    ;; insert
-    (prove:is (treap::treap->list
-               (treap::insert
-                (treap::insert nil 0 11)
-                1
-                10))
-              '(11 10))
-    ;; remove
-    (prove:is (treap::treap->list (treap::remove l 0))
-              nil)
-    (prove:is (treap::treap->list
-               (treap::remove ex1 2))
-              '(1 4 3))
-    ;; ref
-    (prove:is (treap::ref l 1) 1)
-    (prove:is (treap::ref r 1) 2)))
+#+swank (rove:run-suite *package*)
--- treap.lisp
+++ treap.lisp
@@ -4,7 +4,17 @@
   (:use #:cl)
   (:shadow #:merge
            #:remove
-           #:ref))
+           #:ref)
+  (:export #:list->treap
+           #:treap->list
+           #:%get-cnt
+           #:%get-sum
+           #:%plus-cnt
+           #:%plus-sum
+           #:merge
+           #:split
+           #:insert
+           #:remove))
 
 (in-package  #:treap)
 
@@ -146,89 +156,4 @@
     (and center
          (treap-value center))))
 
-#+swank
-(rove:deftest test-treap
-  (let* ((xs (loop repeat 5 collect (random 100)))
-         (ys (loop repeat 10 collect (random 100)))
-         (zs (loop repeat 20 collect (random 100)))
-         (ws (list 1 3 5 7 10))  ; sum = 26
-         (rs (list 1 5 4 3 2))   ; sum = 15
-         (xs-treap (list->treap xs))
-         (ys-treap (list->treap ys))
-         (zs-treap (list->treap zs))
-         (ws-treap (list->treap ws))
-         (rs-treap (list->treap rs))
-         (null-treap (list->treap nil)))
-    (rove:testing "Testing equality"
-      (flet ((convert (list)
-               (treap->list (list->treap list))))
-        (rove:ok (equal (convert xs)
-                        xs))))
-    (rove:testing "get-cnt"
-      (rove:ok (= (%get-cnt ys-treap)
-                  10)
-               "normal")
-      (rove:ok (= (%get-cnt null-treap)
-                  0)
-               "null-treap"))
-    (rove:testing "get-sum"
-      (rove:ok (= (%get-sum ws-treap)
-                  26))
-      (rove:ok (= (%get-sum null-treap)
-                  0)
-               "null-treap"))
-    (rove:testing "plus-cnt"
-      (rove:ok (= (%plus-cnt xs-treap
-                             ys-treap)
-                  15)
-               "normal")
-      (rove:ok (= (%plus-cnt xs-treap
-                             null-treap)
-                  5)
-               "one is null"))
-    (rove:testing "plus-sum"
-      (rove:ok (= (%plus-sum ws-treap rs-treap)
-                  41)
-               "normal")
-      (rove:ok (= (%plus-sum ws-treap null-treap)
-                  26)
-               "one is null"))
-    (rove:testing "merge"
-      (rove:ok (equal (treap->list (merge ws-treap rs-treap))
-                      (concatenate 'list ws rs))
-               "Merge two treap preserving order"))
-    (rove:testing "split"
-      (rove:ok (every #'equal
-                      (mapcar #'treap->list
-                              (multiple-value-list
-                               (split ws-treap 2)))
-                      '((1 3)
-                        (5 7 10)))
-               "Split at 2")
-      (rove:ok (every #'equal
-                      (mapcar #'treap->list
-                              (multiple-value-list
-                               (split ws-treap 3)))
-                      '((1 3 5)
-                        (7 10)))
-               "Split at 3"))
-    (rove:testing "insert"
-      (rove:ok (equal (treap->list (insert ws-treap
-                                           2
-                                           11))
-                      '(1 3 11 5 7 10))
-               "Insert 11 at 2")
-      (rove:ok (equal (treap->list (insert ws-treap
-                                           3
-                                           11))
-                      '(1 3 5 11 7 10))
-               "Insert 11 at 3"))
-    #+nil
-    (rove:testing "remove"
-      (rove:ok (equal (treap->list (remove ws-treap 2))
-                      '(1 3 7 10))))))
-
-#+swank
-(rove:run-suite *package*)
-
-(in-package #:cl-user)
+#+swank (load (merge-pathnames "test/treap.lisp" (truename ".")))
