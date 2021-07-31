dd9687c8bb57002ac9834cdb5d76a267127ffdab
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-31 09:33:43 +0900

    Add test

--- test/treap.lisp
+++ test/treap.lisp
@@ -7,8 +7,23 @@
 
 (in-package :test/treap)
 
+(defun make-fixture-from-list (list)
+  (list->treap ))
+
 #+swank
 (rove:deftest test-treap
-  nil)
+  (rove:testing "merge"
+    (let* ((xs (list 1 4 3))
+           (ys (list 3 7 9))
+           (xs-tr (list->treap xs))
+           (ys-tr (list->treap ys)))
+      (rove:ok (equal (treap->list (merge xs-tr ys-tr))
+                      '(1 4 3 3 7 9))))
+    (let* ((xs (list 1 4 3))
+           (ys (list 3 7 9))
+           (xs-tr (list->treap xs))
+           (ys-tr (list->treap ys)))
+      (rove:ok (equal (treap->list (merge ys-tr xs-tr))
+                      '(3 7 9 1 4 3))))))
 
 #+swank (rove:run-suite *package*)
