0345d8ad083a561e1ed0e53acdd9d2773fd864b6
motoshira <51407064+motoshira@users.noreply.github.com>
2021-08-04 07:22:32 +0900

    Refine skew-heap

--- skew-heap.lisp
+++ skew-heap.lisp
@@ -5,13 +5,30 @@
 ;; Skew Heap
 ;; Reference: http://hos.ac/blog/#blog0001
 
-(defstruct (heap (:constructor make-heap (val &key l r)))
-  (val 0 :type list)
+(defstruct (heap (:constructor make-heap (key value &key l r)))
+  (key key :type fixnum)
+  (value value :type t)
   (l nil :type (or null heap))
   (r nil :type (or null heap)))
 
+(defun heap->list (heap)
+  (let ((res nil))
+    (labels ((%traverse (node)
+               (when node
+                 (with-slots (key value l r)
+                     node
+                   (push (list key value) res)
+                   (%traverse l)
+                   (%traverse r)))))
+      (%traverse heap)
+      (reverse res))))
+
 (defun heap-empty-p (heap)
   (null heap))
+
+(defun heap-peak (heap)
+  (values (heap-key heap)
+          (heap-value heap)))
 
 (defun heap-meld (l r)
   (declare ((or null heap) l r))
@@ -19,29 +36,31 @@
        (cond
          ((null l) r)
          ((null r) l)
-         (:else (when (> (heap-val l) (heap-val r))
+         (:else (when (> (heap-key l) (heap-key r))
                   (rotatef l r))
                 (setf (heap-r l) (heap-meld (heap-r l) r))
                 (rotatef (heap-l l) (heap-r l))
                 l))))
 
-(defmacro heap-push! (heap item)
+(defmacro heap-push! (heap key value)
   (multiple-value-bind (args argvs val setter getter)
       (get-setf-expansion heap)
     `(let ,(mapcar #'list args argvs)
-       (let ((,@val (heap-meld (make-heap ,item) ,getter)))
+       (let ((,@val (heap-meld (make-heap ,key ,value) ,getter)))
          ,setter))))
 
 (defmacro heap-pop! (heap)
-  (let ((item (gensym)))
+  (let ((key (gensym))
+        (value (gensym)))
     (multiple-value-bind (args argvs val setter getter)
         (get-setf-expansion heap)
       `(let ,(mapcar #'list args argvs)
-         (let ((,item (heap-val ,getter))
-               (,@val (heap-meld (heap-l ,getter)
+         (let ((,@val (heap-meld (heap-l ,getter)
                                  (heap-r ,getter))))
-           ,setter
-           ,item)))))
+           (multiple-value-bind (,key ,value)
+               (heap-peak ,getter)
+             ,setter
+             (values ,key ,value)))))))
 
 ;;;
 ;;; EOF
