9b5fee197cce283f5ef396a6388f7c5a175755ca
motoshira <51407064+motoshira@users.noreply.github.com>
2021-08-17 21:20:38 +0900

    Add sd

---
+++ square-root-decomposition.lisp
@@ -0,0 +1,8 @@
+(defpackage squqre-root-decomposition
+  (:use #:cl)
+  (:nicknames #:sd)
+  (:export #:make-square-root-decomposition
+           #:update!
+           #:fold))
+
+(in-package #:sd)
