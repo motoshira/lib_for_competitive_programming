1e0f7ddaac3eea5469a83b90e9dbafc65e72f83d
motoshira <51407064+motoshira@users.noreply.github.com>
2021-09-05 01:20:01 +0900

    Add basic fns

--- experimental/splay-tree.lisp
+++ experimental/splay-tree.lisp
@@ -25,3 +25,50 @@
 
 (defmethod print-object ((obj node) s)
   (princ (dump obj) s))
+
+(defun splay! (node)
+  (symbol-macrolet ((p (node-parent node))
+                    (pp (node-parent p))
+                    (l (node-l node))
+                    (r (node-r node)))
+    (loop while p do
+      (cond
+        ((null pp)
+         (if (eq (node-l p)
+                 node)
+             (zig! node :right)
+             (zig! node :left)))
+        ((eq (node-l p)
+             node)
+         (if (eq (node-l pp)
+                 p)
+             (zig-zig! node :right)
+             (zig-zag! node :right)))
+        (:else
+         (if (eq (node-r pp)
+                 p)
+             (zig-zig! pp :left)
+             (zig-zag! pp :left)))))))
+
+(defun toggle (dir)
+  (ecase dir
+    (:left :right)
+    (:right :left)))
+
+(defun zig! (node dir)
+  (symbol-macrolet ((p (node-parent node)))
+    (ecase dir
+      (:right (setf (node-l p) (node-r node)
+                    (node-r node) p))
+      (:left (setf (node-r p) (node-l node)
+                   (node-l node) p)))))
+
+(defun zig-zig! (node dir)
+  ;; rotate parent -> rotate node
+  (zig! (node-parent node) dir)
+  (zig! node dir))
+
+(defun zig-zag! (node dir)
+  ;; dir is direction of rotate at the first time
+  (zig! node dir)
+  (zig! node (toggle dir)))
