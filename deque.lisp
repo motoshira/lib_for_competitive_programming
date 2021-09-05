b601fcc258a34dc90298231a49be43650d7c44fb
motoshira <51407064+motoshira@users.noreply.github.com>
2021-09-05 23:11:20 +0900

    Refactor deque

--- deque.lisp
+++ deque.lisp
@@ -1,14 +1,23 @@
+;;;
+;;; BOF
+;;;
 
-(defparameter *default-deque-size* 100)
+;; deque
 
-(defstruct (deque (:conc-name deq-)
-                  (:constructor %make-deque))
-  (data nil :type array)
+(defpackage #:deque
+  (:use #:cl)
+  (:nicknames #:deq))
+
+(in-package #:deque)
+
+(defparameter *default-deque-size* 300000)
+
+(defstruct (deque (:constructor %make-deque))
+  (data nil :type (simple-array t (*)))
   (size nil :type fixnum)
   (head 0 :type fixnum)
   (tail 0 :type fixnum)
   (count 0 :type fixnum))
-
 
 (defun make-deque (&optional (size *default-deque-size*))
   (%make-deque
@@ -18,90 +27,73 @@
 
 
 (defmethod deque-clear ((d deque))
-  (fill (deque-data d) 0)
   (setf (deque-size d) 0)
   (setf (deque-head d) 0)
   (setf (deque-tail d) 0)
   (setf (deque-count d) 0))
 
-;; Subcommand
+(declaim (inline empty-p
+                 full-p
+                 get-prev-index
+                 get-next-index))
 
-(declaim (inline deque-empty-p
-                 deque-full-p
-                 deque-get-prev-index
-                 deque-get-next-index))
-
-
-(defmethod deque-empty-p ((d deque))
+(defmethod empty-p ((d deque))
   (zerop (deque-count d)))
 
-
-
-(defmethod deque-full-p ((d deque))
+(defmethod full-p ((d deque))
   (= (deque-count d) (deque-size d)))
 
-(defmethod deque-get-prev-index ((d deque) idx)
+(defmethod get-prev-index ((d deque) idx)
   (declare (inline deque-get-next-index))
   (if (zerop idx)
       (1- (deque-size d))
       (1- idx)))
 
-(defmethod deque-get-next-index ((d deque) idx)
+(defmethod get-next-index ((d deque) idx)
   (rem (1+ idx) (deque-size d)))
 
-
-
-
-;;; Main command
-
-(defmethod deque-pushfront ((d deque) item)
-  (when (deque-full-p d)
+(defmethod push-front! ((d deque) item)
+  (when (full-p d)
     (error "deque is full"))
-
-  (setf (deque-head d) (deque-get-prev-index d (deque-head d)))
+  (setf (deque-head d) (get-prev-index d (deque-head d)))
   (setf (aref (deque-data d) (deque-head d)) item)
-  (when (deque-empty-p d) ; first insersion
+  (when (empty-p d) ; first insersion
     (setf (deque-tail d) (deque-head d)))
   (incf (deque-count d)))
 
-
-
-(defmethod deque-pushback ((d deque) item)
-  (when (deque-full-p d)
+(defmethod push-back! ((d deque) item)
+  (when (full-p d)
     (error "deque is full"))
-  
-  (setf (deque-tail d) (deque-get-next-index d (deque-tail d)))
+  (setf (deque-tail d) (get-next-index d (deque-tail d)))
   (setf (aref (deque-data d) (deque-tail d)) item)
-  (when (deque-empty-p d) ; first insersion
+  (when (empty-p d) ; first insersion
     (setf (deque-head d) (deque-tail d)))
   (incf (deque-count d)))
 
-
-(defmethod deque-popfront ((d deque))
-  (when (deque-empty-p d)
+(defmethod pop-front! ((d deque))
+  (when (empty-p d)
     (error "deque is empty,"))
-  
   (let ((value (aref (deque-data d) (deque-head d))))
-    (setf (deque-head d) (deque-get-next-index d (deque-head d)))
+    (setf (deque-head d) (get-next-index d (deque-head d)))
     (decf (deque-count d))
     value))
 
-
-
-(defmethod deque-popback ((d deque))
-  (when (deque-empty-p d)
+(defmethod pop-back! ((d deque))
+  (when (empty-p d)
     (error "deque is empty,"))
-  
   (let ((value (aref (deque-data d) (deque-tail d))))
-    (setf (deque-tail d) (deque-get-prev-index d (deque-tail d)))
+    (setf (deque-tail d) (get-prev-index d (deque-tail d)))
     (decf (deque-count d))
     value))
 
 
-(defmethod dref ((d deque) subscripts)
+(defmethod dref ((d deque) index)
   (let ((arr (deque-data d))
         (size (deque-size d))
         (head (deque-head d)))
-    (aref arr (mod (+ subscripts
-                      head)
+    (aref arr (mod (+ index head)
                    size))))
+
+;;;
+;;; EOF
+;;;
