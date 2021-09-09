dd33d3428dda38d8f7eea6080cdaacc5888b48a1
motoshira <51407064+motoshira@users.noreply.github.com>
2021-09-10 00:03:29 +0900

    Fix skew-heap

--- skew-heap.lisp
+++ skew-heap.lisp
@@ -9,7 +9,7 @@
   (:use #:cl)
   (:nicknames #:sk)
   (:export #:heap
-           #:make-heap
+           #:%make-heap
            #:heap->list
            #:empty-p
            #:meld
@@ -27,22 +27,29 @@
   (declare (value-container value-container))
   (value-container-value value-container))
 
-(defstruct (heap (:constructor make-heap (key value &key l r)))
+(defstruct (heap (:constructor %make-heap (key val l r)))
   (key key :type fixnum)
-  (value (make-value :value value) :type value-container)
+  (value (make-value-container :value val) :type value-container)
   (l nil :type (or null heap))
   (r nil :type (or null heap)))
 
 (defun heap->list (heap)
-  (let ((res nil)
-        (h heap))
-    (loop until (empty-p h)
-          do (push (list (heap-key h)
-                         (unwrap (heap-value h)))
-                   res)
-             (setf h (meld (heap-l h)
-                           (heap-r h))))
-    (reverse res)))
+  (declare (heap heap))
+  (labels ((rec (h acc)
+             (if (null h)
+                 (reverse acc)
+                 (rec (meld (heap-l h)
+                            (heap-r h)
+                            :comparator #'default-comparator)
+                      (cons (list (heap-key h)
+                                  (unwrap (heap-value h)))
+                            acc)))))
+    (rec heap nil)))
+
+(defmethod print-object ((obj heap) s)
+  (print-unreadable-object (obj s)
+    (princ "HEAP" s)
+    (princ (heap->list obj) s)))
 
 (defun empty-p (heap)
   (null heap))
@@ -51,40 +58,51 @@
   (if (null heap)
       (values nil nil)
       (values (heap-key heap)
-              (heap-value heap))))
+              (unwrap (heap-value heap)))))
 
-(defun meld (l r &key (comparator (lambda (x y)
-                                    (declare (fixnum x y))
-                                    (< x y))))
+(defun default-comparator (x y)
+  (declare (fixnum x y))
+  (< x y))
+
+(defun meld (l r &key (comparator #'default-comparator))
   (declare ((or null heap) l r)
            ((function (fixnum fixnum) boolean) comparator))
   (the (or null heap)
        (cond
          ((null l) r)
          ((null r) l)
-         (:else (when (funcall (complement comparator)
-                               (heap-key l)
-                               (heap-key r))
-                  (rotatef l r))
-                (setf (heap-r l) (meld (heap-r l) r))
-                (rotatef (heap-l l) (heap-r l))
-                l))))
+         ((funcall comparator (heap-key l) (heap-key r))
+          (%make-heap (heap-key l)
+                      (unwrap (heap-value l))
+                      (meld (heap-r l)
+                            r
+                            :comparator comparator)
+                      (heap-l l)))
+         (:else
+          (%make-heap (heap-key r)
+                      (unwrap (heap-value r))
+                      (heap-r r)
+                      (meld l
+                            (heap-l r)
+                            :comparator comparator))))))
 
-(defmacro push! (heap key value)
+(defmacro push! (heap key value &key (comparator 'default-comparator))
   (multiple-value-bind (args argvs val setter getter)
       (get-setf-expansion heap)
     `(let ,(mapcar #'list args argvs)
-       (let ((,@val (meld (make-heap ,key ,value) ,getter)))
+       (let ((,@val (meld (%make-heap ,key ,value nil nil) ,getter :comparator #',comparator)))
          ,setter))))
 
-(defmacro pop! (heap)
+(defmacro pop! (heap &key (comparator 'default-comparator))
   (let ((key (gensym))
         (value (gensym)))
     (multiple-value-bind (args argvs val setter getter)
         (get-setf-expansion heap)
       `(let ,(mapcar #'list args argvs)
          (let ((,@val (meld (when ,getter (heap-l ,getter))
-                            (when ,getter (heap-r ,getter)))))
+                            (when ,getter (heap-r ,getter))
+                            :comparator #',comparator)))
+
            (multiple-value-bind (,key ,value)
                (peak ,getter)
              ,setter
