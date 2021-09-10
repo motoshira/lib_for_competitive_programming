c94df04b3902a7e6ecf07266c5918e5002803a93
motoshira <51407064+motoshira@users.noreply.github.com>
2021-09-10 22:54:19 +0900

    Fix modint

--- modint.lisp
+++ modint.lisp
@@ -11,8 +11,7 @@
            #:-
            #:*
            #:/)
-  (:export #:mint
-           #:modint
+  (:export #:modint
            #:+
            #:-
            #:*
@@ -25,60 +24,47 @@
 (define-symbol-macro *mod* 1000000007)
 ;; (define-symbol-macro *mod* 998244353)
 
-(deftype mint () `(integer 0 #.(1- *mod*)))
-
 (declaim (inline modint)
-         (ftype (function (integer) mint) modint))
+         (ftype (function (integer) fixnum) modint))
 (defun modint (integer)
-  ;; (integer) -> (mint)
+  ;; (integer) -> (fixnum)
   ;; TODO TLE on big negative integer
   (declare (integer integer))
   (loop while (minusp integer)
         do (incf integer *mod*))
-  (the mint
+  (the fixnum
        (if (< integer *mod*)
            integer
            (mod integer *mod*))))
 
-(defmacro define-modulo-operation (fn-name op-long op-short)
-  `(progn
-     ;; (&REST mint) -> (mint)
-     (declaim (ftype (function (&rest mint) mint) ,fn-name)
-              (inline ,fn-name))
-     (defun ,fn-name (&rest args)
-       (reduce (lambda (x y)
-                 ,op-long)
-               (rest args)
-               :initial-value (first args)))
+(defun %operate (fn &rest args)
+  (declare ((function (fixnum fixnum) fixnum) fn))
+  (if (null args)
+      0
+      (reduce (lambda (x y)
+                (modint (funcall fn x (modint y))))
+              (rest args)
+              :initial-value (first args))))
 
-     (define-compiler-macro ,fn-name (&whole form &rest args)
-       (if (< (length args) 10)
-           (reduce (lambda (x y)
-                     ,op-short)
-                   (rest args)
-                   :initial-value (first args))
-           form))))
+(defun + (&rest args)
+  (%operate cl:+ args))
 
+(defun - (&rest args)
+  (%operate cl:- args))
 
+(defun * (&rest args)
+  (%operate cl:* args))
 
-(define-modulo-operation m:+
-    (modint (cl:+ (the fixnum x)
-                  (the fixnum y)))
-  `(modint (cl:+ (the fixnum ,x)
-                 (the fixnum ,y))))
-(define-modulo-operation m:-
-    (modint (cl:- (the fixnum x)
-                  (the fixnum y)))
-  `(modint (cl:- (the fixnum ,x)
-                 (the fixnum ,y))))
-(define-modulo-operation m:* (modint (cl:* x y)) `(modint (cl:* ,x ,y)))
-(define-modulo-operation m:/ (modint (cl:* x (mod-inv y))) `(modint (cl:* ,x (mod-inv ,y))))
+(defun / (&rest args)
+  (%operate (lambda (x y)
+              (* x (mod-inv y)))
+            args))
 
-(declaim (ftype (function (fixnum) mint) mod-inv))
+(declaim (ftype (function (fixnum) fixnum) mod-inv))
 (defun mod-inv (a)
   "Reference:https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a"
   (declare (fixnum a)
-           (optimize (speed 3) (safety 2)))
+           (optimize (speed 3)))
   (let ((b *mod*)
         (u 1)
         (v 0))
@@ -97,53 +83,53 @@
         (rotatef u v)))
     (modint u)))
 
-(declaim (ftype (function (mint (integer 0)) mint) mod-power)
+(declaim (ftype (function (fixnum (integer 0)) fixnum) mod-power)
          (inline mod-power))
 (defun mod-power (base power)
   ;; Reference:https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
-  (declare (mint base)
+  (declare (fixnum base)
            ((integer 0) power))
   (loop while (plusp power)
-        with res of-type mint = 1
-        do (psetq base (the mint (mod* base base))
+        with res of-type fixnum = 1
+        do (psetq base (the fixnum (* base base))
                   power (the (integer 0) (ash power -1))
-                  res (the mint (if (logbitp 0 power)
-                                    (mod* res base)
+                  res (the fixnum (if (logbitp 0 power)
+                                    (* res base)
                                     res)))
         finally (return res)))
 
-(define-modify-macro incmodf (&optional (val 1)) (lambda (place val) (mod+ place val)))
-(define-modify-macro decmodf (&optional (val 1)) (lambda (place val) (mod- place val)))
-(define-modify-macro mulmodf (&optional (val 1)) (lambda (place val) (mod* place val)))
-(define-modify-macro divmodf (&optional (val 1)) (lambda (place val) (mod/ place val)))
+(define-modify-macro incmodf (&optional (val 1)) (lambda (place val) (+ place val)))
+(define-modify-macro decmodf (&optional (val 1)) (lambda (place val) (- place val)))
+(define-modify-macro mulmodf (&optional (val 1)) (lambda (place val) (* place val)))
+(define-modify-macro divmodf (&optional (val 1)) (lambda (place val) (/ place val)))
 
-(declaim (ftype (function (mint) (simple-array mint (*))) make-mod-table)
+(declaim (ftype (function (fixnum) (simple-array fixnum 1)) make-mod-table)
          (inline make-mod-fact-table))
 (defun make-mod-fact-table (size)
-  (declare (mint size))
+  (declare (fixnum size))
   (let ((table (make-array (1+ size)
-                           :element-type 'mint)))
-    (declare ((simple-array mint (*)) table))
+                           :element-type 'fixnum)))
+    (declare ((simple-array fixnum 1) table))
     (setf (aref table 0) 1)
     (loop for i of-type fixnum below size
           do (setf (aref table (1+ i))
-                   (mod* (aref table i)
-                         (the mint (1+ i)))))
+                   (* (aref table i)
+                      (the fixnum (1+ i)))))
     table))
 
-(declaim (ftype (function (mint mint (simple-array mint (*))) mint) mod-combi-with-table)
+(declaim (ftype (function (fixnum fixnum (simple-array fixnum 1)) fixnum) mod-combi-with-table)
          (inline mod-combi-with-table))
 (defun mod-combi-with-table (n k table)
-  (declare (mint n k)
-           ((simple-array mint (*)) table))
-  (the mint
+  (declare (fixnum n k)
+           ((simple-array fixnum 1) table))
+  (the fixnum
        (if (or (< n k)
                (< n 0)
                (< k 0))
            0
-           (mod* (aref table n)
-                 (mod-inv (aref table k))
-                 (mod-inv (aref table (the mint (- n k))))))))
+           (* (aref table n)
+              (mod-inv (aref table k))
+              (mod-inv (aref table (the fixnum (- n k))))))))
 
 
 ;;;
